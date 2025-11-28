22fe337f6da8d0dd02c55821a5b1f3bc895e1115
DanuloM <danulo1337@gmail.com>
2025-11-28 17:55:06 +0200

    solution4

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -92,8 +92,9 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 
